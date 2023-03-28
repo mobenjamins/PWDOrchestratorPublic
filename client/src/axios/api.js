db3372367706f0ee55bdf4521b8ca958e0a2eb02
Kipsanga-Gregory <greglimo366@yahoo.com>
2023-03-28 16:36:40 +0300

    Added key with one year validity

--- client/src/axios/api.js
+++ client/src/axios/api.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 export const sirenAPI = axios.create({
-  baseURL: 'https://api.insee.fr/entreprises/sirene/V3',
-  headers: {
-    'Authorization': 'Bearer eb27f691-2e09-39d5-a03e-c7c2c6a803f3',
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  }
+   baseURL: 'https://api.insee.fr/entreprises/sirene/V3',
+   headers: {
+      Authorization: 'Bearer 938c22af-caa1-3b20-b852-8b271a593105',
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+   }
 });
- 
